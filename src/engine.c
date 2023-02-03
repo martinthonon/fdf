d080ead5a69e0b3202affa54a81f91c003600573
martinthonon <mrt.thonon@gmail.com>
2023-02-03 10:59:14 +0100

    struct grid implemented

--- src/engine.c
+++ src/engine.c
@@ -2,26 +2,33 @@
 
 
 
-void ft_grid_size(int fd, t_grid *grid)
+int ft_grid_size(int fd, t_grid *grid)
 {
     char *gnl;
     char **split;
+    int i;
 
     grid->width = 0;
     gnl = ft_get_next_line(fd);
+    if (!gnl)
+        return (1);
     while (gnl)
     {
         grid->length = 0;
         split = ft_split(gnl, ' ');
-        while (*split)
+        if (!split)
+            return (ft_free("%p", gnl) + 1);
+        i = -1;
+        while (split[++i])
         {
-            if (**split != '\n')
+            if (split[i][0] != '\n')
                 grid->length++;
-            ++split;
         }
         grid->width++;
+        ft_free("%p, %P", gnl, split);
         gnl = ft_get_next_line(fd);
     }
+    return (ft_ret(NULL, fd, 0));
 }
 
 int ft_engine_init(char *str)
@@ -32,7 +39,8 @@ int ft_engine_init(char *str)
     fd = open(str, O_RDONLY);
     if (fd < 0)
         return (1);
-    ft_grid_size(fd, &grid);
+    if (ft_grid_size(fd, &grid))
+        return (ft_ret(NULL, fd, 1));
     printf("length -> %d, width -> %d\n", grid.length, grid.width);
 
     return (0);
